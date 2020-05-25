2d4239a41fa71b6faddf3c8af172e8bed93e0868
tcdale <tom55dale@gmail.com>
2020-05-25 17:51:00 +0100

    Update

--- docker-compose/db_post_create_scripts/1_change_param.sql
+++ docker-compose/db_post_create_scripts/1_change_param.sql
@@ -1,4 +1,4 @@
 ALTER SYSTEM SET shared_preload_libraries='pg_stat_statements';
 ALTER SYSTEM SET max_connections=1000;
 ALTER SYSTEM SET track_activity_query_size=1000;
-ALTER SYSTEM SET shared_buffers='1024MB';
+ALTER SYSTEM SET shared_buffers='2048MB';

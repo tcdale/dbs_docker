fc40f6435e0baf51d0156a2420d79fe41e6e6877
tcdale <tom55dale@gmail.com>
2020-06-30 10:59:56 +0100

    Update

--- docker-build-dbs-core/build_and_push.sh
+++ docker-build-dbs-core/build_and_push.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Rebuild container"
-#docker build --no-cache -t dbs-core .
-docker build -t dbs-core .
+docker build --no-cache -t dbs-core .
+#docker build -t dbs-core .
 echo "Tag"
 docker tag dbs-core tomdale55/dbs-core
-echo "Push"
-docker push tomdale55/dbs-core
+#echo "Push"
+#docker push tomdale55/dbs-core
